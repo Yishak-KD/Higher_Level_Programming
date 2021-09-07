b837bc0484d37f391cbc0d8e271726ccbde363fa
Yishak-KD <yisak04@gmail.com>
2021-09-07 06:55:46 -0700

    Print lower character except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i == ord('e') or i == ord('q'):
-        continue
-    else:
-        print(chr(i), end="")
+for sletter in range(97, 123):
+    if chr(sletter) is not 'q' and chr(sletter) is not 'e':
+        print("{}".format(chr(sletter)), end="")
