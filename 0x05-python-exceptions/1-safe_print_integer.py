3195879d5fc65934d40a950ae9c8ae26e01a1a56
Yishak-KD <yisak04@gmail.com>
2021-10-07 23:30:09 -0700

    Function that prints an integer with string literals, try/except

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-
 def safe_print_integer(value):
-
-    try:
-        print("{:d}".format(value))
-        return True
-    except:
+    if isinstance(value, int):
+        try:
+            print("{:d}" .format(value))
+            return True
+        except:
+            pass
+    else:
         return False
+    print()
