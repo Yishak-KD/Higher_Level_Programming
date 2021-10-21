ca75bf1c6c8e7984cd3ae3056405ea5f6bdc06e8
Yishak-KD <yisak04@gmail.com>
2021-10-21 01:06:52 -0700

    Class rectangle, with a width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,36 +3,40 @@
 
 class Rectangle:
     """A class of rectangle"""
+
     def __init__(self, width=0, height=0):
         """Initializing the class attributes
             Args:
                 width: value of the width
                 height: value of the height
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Return a value from the width"""
+        """Return a value from the width."""
         return self.__width
+
     @width.setter
     def width(self, value):
-        """Set a value property"""
-        if isinstance(value, int):
+        """Set the value using the setter property."""
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """Return a value from the height"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """Set a value for the height"""
-        if isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
