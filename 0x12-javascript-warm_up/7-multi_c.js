5e788d08143bb973357b8ecd0360e02bace6d923
Yishak Kidanu <yisak04@gmail.com>
2022-03-01 14:18:28 +0000

    Print to the console number of argument times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require("process");
+
+if (!isNaN(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurences");
+}
