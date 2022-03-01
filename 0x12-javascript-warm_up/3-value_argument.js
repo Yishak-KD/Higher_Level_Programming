c5d9ddc8cbd2315bd61dfc843f759fb2927cb52c
Yishak Kidanu <yisak04@gmail.com>
2022-03-01 13:18:15 +0000

    Print argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 const { argv } = require('process');
 
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
     console.log("No argument");
 } else {
     console.log(process.argv[2]);
