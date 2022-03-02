ec7c7a04a65ed83d92aa9ebefc2c7b02bf3670f3
Yishak Kidanu <yisak04@gmail.com>
2022-03-02 14:55:09 +0000

    Function that returns number of occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // Return the number of occurrences
-exports.nbOccurences;
-
-function nbOccurences(list, searchElement) {
+exports.nbOccurences = function nbOccurences(list, searchElement) {
     let count = 0;
     for (let i = 0; i < list.length; i++) {
         if (list[i] === searchElement) {
