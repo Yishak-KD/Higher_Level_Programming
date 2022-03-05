28581c1c38f7e32202d3c0501fdd064964cc5cfc
Yishak Kidanu <yisak04@gmail.com>
2022-03-05 07:13:18 -0800

    Script that lists a specific alphabet from a database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Script that lists all the names that starts with a specific alphabet
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    my_cursor = db.cursor()
+
+    my_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for m in my_cursor.fetchall():
+        print(m)
