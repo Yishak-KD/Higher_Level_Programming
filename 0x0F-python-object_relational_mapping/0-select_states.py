63a28258798d66f1ce6587cad7080e83aea84658
Yishak Kidanu <yisak04@gmail.com>
2022-03-05 03:32:19 -0800

    Python script that lists all details from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+import sys
+import MySQLdb
+
+db = MySQLdb.connect(
+    user='sys.argv[1]',
+    passwd='sys.argv[2]',
+    database='sys.argv[3]'
+)
+
+mycursor = db.cursor()
+
+mycursor.execute("SELECT * FROM states ORDER_BY id ASC")
+for i in mycursor.fetchall():
+    print(i)
+
