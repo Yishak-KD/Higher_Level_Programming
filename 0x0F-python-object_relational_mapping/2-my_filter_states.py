69f03d1fb4be689cb17614c42747af215bca9046
Yishak Kidanu <yisak04@gmail.com>
2022-03-06 05:25:39 -0800

    script that takes in an argument and displays all values

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Script that lists all the names that starts with a specific alphabet
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    my_cursor = db.cursor()
+
+    my_cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4]))
+    for m in my_cursor.fetchall():
+        print(m)
