f611f8758d7bb3cb660029ea2191b2fdf790ff1e
Yishak Kidanu <yisak04@gmail.com>
2022-03-07 01:37:28 -0800

    SQL join and take arguments to list

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ Takes an argument and searches from the table to list city names
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
@@ -15,9 +15,7 @@ if __name__ == '__main__':
 
     mycursor = db.cursor()
 
-    mycursor.execute("SELECT cities.name FROM states JOIN cities ON states.id = cities.state_id WHERE cities.name='{}' ORDER BY cities.id ASC".format([sys.argv[4]]))
-    for i in mycursor.fetchall:
+    mycursor.execute("SELECT cities.name FROM states INNER JOIN cities ON states.id = cities.state_id WHERE cities.name='{}'".format(sys.argv[4]))
+    for i in mycursor.fetchall():
         if sys.argv[4] == states.name:
             print(i)
-        else:
-            print()
