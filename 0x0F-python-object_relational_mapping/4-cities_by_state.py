1b303ce1bbaf4a0080f4dd2b8e10c3d668b9973e
Yishak Kidanu <yisak04@gmail.com>
2022-03-07 00:53:13 -0800

    Join SQL in python

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pyhton3
+'''
+SQL joins in Python + Mysql
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    mycursor = db.cursor()
+
+    mycursor.execute("SELECT cities.id, cities.name, states.name FROM states JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC")
+    for i in mycursor.fetchall:
+        print(i)
