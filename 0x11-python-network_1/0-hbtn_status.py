06414b6d5baa5ad1a4efc84282bf42d986aed6c5
root <Yishak Kidanu>
2022-03-22 07:57:07 -0700

    Fetching url in Python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-# Fetch url using python script
+"""Fetch url using python script"""
 
 import urllib.request
 if __name__ == '__main__':
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read().decode("utf8")
-        print(html)
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
