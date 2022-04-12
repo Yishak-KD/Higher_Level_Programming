f1a3878a08e5d163caeabc63ebc89c7cbf470345
root <Yishak Kidanu>
2022-04-12 11:08:37 -0700

    Script that takes a URL, and sends a POST request to the passed URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,11 @@ import urllib.parse
 import sys
 
 if __name__ == '__main__':
-    value = {"Email": sys.argv[2]}
+    value = {"email": sys.argv[2]}
 
     data = urllib.parse.urlencode(value).encode('ascii')
 
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+        x = response.read().decode('utf-8')
+        print(x)
