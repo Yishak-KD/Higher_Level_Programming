ca87cddc5a4ba32f45a4a700d938bec9c9a1d7d3
root <Yishak Kidanu>
2022-04-12 11:18:44 -0700

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that takes a URL, display the body of the response
+"""
+
+import urllib.request
+import sys
+import urllib.error
+
+req = urllib.request.Request(sys.argv[1])
+try:
+    with urllib.request.urlopen(req) as resp:
+            print(resp.read().decode("ascii"))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
+
