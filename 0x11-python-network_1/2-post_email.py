14778ec25fceaa25dca8a4f3da9fc3784cfbb56a
root <Yishak Kidanu>
2022-04-12 07:21:36 -0700

    Script that takes URL and Post a request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email, sends POST request
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    value = {'Email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req, data) as response:
+        print(response.read().decode('utf-8'))
