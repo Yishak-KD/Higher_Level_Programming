86d89885aea47be28e4bc25231f28f519ad37156
root <Yishak Kidanu>
2022-04-12 06:06:10 -0700

    Script that takes a URL, and displays result of X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,4 +9,4 @@ import sys
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
-        print(response.headers.get('X-Request-Id'))
+        print(dict(response.headers).get('X-Request-Id'))
