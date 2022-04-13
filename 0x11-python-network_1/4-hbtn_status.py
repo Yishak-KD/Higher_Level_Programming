c92c4b1d2967336b56e9890e5ffc0cf024acb4a9
root <Yishak Kidanu>
2022-04-13 00:26:14 -0700

    Script that fetches using request module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,4 +5,4 @@ import requests
 req = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
 print("\t- type: {}".format(type(req.text)))
-print("\t- content: {}".format(req.content))
+print("\t- content: {}".format(req.content.decode('utf-8')))
