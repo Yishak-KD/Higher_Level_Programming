04b24c12ffae6a1d0818cb570164edb03cb29776
root <Yishak Kidanu>
2022-04-13 01:24:23 -0700

    Error code with requests module

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-        req = requests.get(url)
+    req = requests.get(url)
     if req.status_code < 400:
         print(req.text)
     else:
