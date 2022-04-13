590847b9e3faf6d775972dae7059c8da76671f33
root <Yishak Kidanu>
2022-04-13 00:34:43 -0700

    Script that takes URL and display the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that takes URL and display header response"""
+
+import requests
+import sys
+
+url = sys.argv[1]
+resp = requests.get(url)
+print(resp.headers.get('X-Request-Id'))
