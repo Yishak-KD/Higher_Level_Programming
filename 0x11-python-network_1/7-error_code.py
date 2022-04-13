075523c190fffbad7c44ff99bf52228a6484d283
root <Yishak Kidanu>
2022-04-13 01:02:40 -0700

    Error code with requests module

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Error code"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        req = requests.get(url)
+        print(req.text)
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(e.code))
