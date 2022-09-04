88b37537cb48be47b9255ea45c1f80cd6f23f507
Yishak T. KIDANU <80487041+Yishak-KD@users.noreply.github.com>
2022-09-04 14:52:24 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-    let string = new Array;
-    for (let i = (list.length - 1); i >= 0; i--) {
-        string.push(list[i])
-    }
-    return string;
-}
--- 0x13-javascript_objects_scopes_closures/README.md
+++
@@ -1,1 +0,0 @@
-JavaScript objects_scopes_closures
