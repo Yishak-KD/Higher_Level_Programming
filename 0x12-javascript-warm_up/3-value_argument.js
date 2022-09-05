f02d877665e168fe549dff3ef03eb466290b906e
Yishak Kidanu <yisak04@gmail.com>
2022-09-05 10:21:53 +0000

    Print first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Print first argument
+
+const process = require('process');
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
