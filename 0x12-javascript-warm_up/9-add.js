e3d66a1e94bd71d41a8e999ba879b04f6c83ebcc
Yishak Kidanu <yisak04@gmail.com>
2022-09-05 15:44:12 +0000

    A script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Print addition of 2 integers
 
-let num1 = parseInt(process.argv[2]);
-let num2 = parseInt(process.argv[3]);
-let sum = num1 + num2;
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+const sum = num1 + num2;
 
 console.log(sum);
