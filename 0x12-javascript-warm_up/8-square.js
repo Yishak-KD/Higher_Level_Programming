3690e18786bfc6e0b8b2d39862ff9d3d9068a617
Yishak Kidanu <yisak04@gmail.com>
2022-09-05 14:37:56 +0000

    A script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Print a square
+
+const process = require('process');
+let string = '';
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < process.argv[2]; i++) {
+  for (let j = 0; j < process.argv[2]; j++) {
+    string += 'X';
+  }
+  string += '\n';
+}
+console.log(string);
