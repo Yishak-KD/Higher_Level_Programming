09e2f3d8b8e8f2dead081a0aacae714df32908d5
Yishak Kidanu <yisak04@gmail.com>
2022-09-05 14:50:58 +0000

    A script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Compute and print factorial
+
+const process = require('process');
+
+function factorial(a) {
+  if(a === 1 || a === undefined) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+
+console.log(factorial(process.argv[2]));
