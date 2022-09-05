0f72e9b771d68409bba35b389471f546cbd963e3
Yishak Kidanu <yisak04@gmail.com>
2022-09-05 11:24:32 +0000

    Script that prints on the console by using an array of string and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Using loops to print on the console
+
+const data = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
