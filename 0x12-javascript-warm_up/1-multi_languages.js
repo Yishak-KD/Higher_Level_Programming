db78a535d3621b0eb47e6853b867a0595b63ccfc
Yishak Kidanu <yisak04@gmail.com>
2022-09-05 08:29:21 +0000

    Print different files on the console

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Print different string types on console
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
