a2b8cef5bebeac87365de697ba0319141c5d23c0
Yishak Kidanu <yisak04@gmail.com>
2022-09-06 12:04:02 +0000

    A class that defines Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+// JS class of a rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let string = '';
+
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        string += 'X';
+      }
+      string += '\n';
+    }
+    console.log(string);
+  }
+
+  rotate () {
+    const temp;
+
+    temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+};
