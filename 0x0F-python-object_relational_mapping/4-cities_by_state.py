107c9fcaef4615c70e81602ace50a1d1ae0060d0
Yishak Kidanu <yisak04@gmail.com>
2022-09-13 12:19:17 +0000

    A script that lists all cities from a given database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+'''
+Script that lists all cities from a given database.
+'''
+
+if __name__ == '__main__':
+	db = MySQLdb.connect(
+	user=sys.argv[1],
+	passwd=sys.argv[2],
+	db=sys.argv[3]
+)
+	mycursor = db.cursor()
+	mycursor.execute("SELECT cities.id, cities.name, states.name\
+	FROM states JOIN cities\
+	ON cities.state_id = states.id\
+	ORDER BY cities.state_id ASC")
+
+	for i in mycursor.fetchall():
+		print(i)
+
+	mycursor.close()
+	db.close()
