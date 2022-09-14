dcdc7ec7d3e629928c1bc328af047dfb515b6fd4
Yishak Kidanu <yisak04@gmail.com>
2022-09-14 12:44:59 +0000

    A script that deletes an object from a given database

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that deletes an object to a database"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+	Session = sessionmaker(bind = engine)
+	session = Session()
+
+	for result in session.query(State):
+		if 'a' in result.name:
+			session.delete(result)
+	session.commit()
