3b4797b19464200f0300c03551831c119fce1bae
Yishak Kidanu <yisak04@gmail.com>
2022-09-14 09:06:17 +0000

    A script that prints the first bject from a given database

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-""" Script that prints the first name"""
+"""Script that prints the first State object from a DB"""
 
 import sys
-from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-	engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-	Session = sessionmaker(bind=engine)
-	
-	session = Session()
+	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-	state = session.query(State).order_by(State.id).first()
-	if state is None:
+	Session = sessionmaker(bind = engine)
+	session = Session()
+	result = session.query(State).first()
+	if result.name == "":
 		print("Nothing")
 	else:
-		print("{}: {}".format(state.id, state.name))
+		print("{}: {}".format(result.id, result.name))
