e94486e6e59bfcf6026a01e27f56d14208ed6ff7
Yishak Kidanu <yisak04@gmail.com>
2022-09-16 06:20:27 -0700

    A script that takes in an argument and displays all values

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,18 +4,19 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(
+    db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
 
-	mycursor = db.cursor()
+    mycursor = db.cursor()
 
-	mycursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4]))
-	
-	for i in mycursor.fetchall():
-		print(i)
+    mycursor.execute(
+        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4]))
 
-	mycursor.close()
-	db.close()
+    for i in mycursor.fetchall():
+        print(i)
+
+    mycursor.close()
+    db.close()
