0cfd2dbad4ddf8f5ffa65c45495e76588c2f676c
Yishak Kidanu <yisak04@gmail.com>
2022-09-16 11:42:59 -0700

    A sscript that lists all cities from a given database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,19 +4,18 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-	db = MySQLdb.connect(
-	user=sys.argv[1],
-	passwd=sys.argv[2],
-	db=sys.argv[3]
-)
-	mycursor = db.cursor()
-	mycursor.execute("SELECT cities.id, cities.name, states.name\
-	FROM states JOIN cities\
-	ON cities.state_id = states.id\
-	ORDER BY cities.state_id ASC")
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    mycursor = db.cursor()
+    mycursor.execute("SELECT cities.id, cities.name, states.name\
+    FROM states JOIN cities\
+    ON cities.state_id = states.id\
+    ORDER BY cities.state_id ASC")
+    for i in mycursor.fetchall():
+        print(i)
 
-	for i in mycursor.fetchall():
-		print(i)
-
-	mycursor.close()
-	db.close()
+    mycursor.close()
+    db.close()
