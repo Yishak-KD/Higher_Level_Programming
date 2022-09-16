36cb8295d7ff70e8246cbd4f652d26ca1fef36df
Yishak Kidanu <yisak04@gmail.com>
2022-09-16 05:43:45 -0700

    A script that lists all states with a name starting with a specific letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,17 +5,18 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(
+    db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-	charset="utf8"
+        charset="utf8"
     )
-	mycursor = db.cursor()
-	mycursor.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id` ASC")
-	
-	for i in mycursor.fetchall():
-		print(i)
+    mycursor = db.cursor()
+    mycursor.execute(
+        "SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id` ASC")
 
-	mycursor.close()
-	db.close()
+    for i in mycursor.fetchall():
+        print(i)
+
+    mycursor.close()
+    db.close()
