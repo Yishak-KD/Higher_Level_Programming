9394ebdfcf3afb26c18906bcb438808b3c57845e
Yishak Kidanu <yisak04@gmail.com>
2022-09-16 11:49:38 -0700

    A script that prints the first State object from a database

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,12 +7,13 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-	Session = sessionmaker(bind = engine)
-	session = Session()
-	result = session.query(State).first()
-	if result is None:
-		print("Nothing")
-	else:
-		print("{}: {}".format(result.id, result.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).first()
+    if result is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(result.id, result.name))
