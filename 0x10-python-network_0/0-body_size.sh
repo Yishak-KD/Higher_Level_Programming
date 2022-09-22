f7313b031868719dd32fc34fee1480a13793e246
Yishak Kidanu <yisak04@gmail.com>
2022-09-22 04:17:27 -0700

    A script that takes a URL, sends a request and display the size of the body response.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # A script that takes in a URL, and display size of body response
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | cut -d ':' -f2
