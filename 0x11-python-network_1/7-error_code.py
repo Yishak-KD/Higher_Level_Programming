2c98be205cefcbb39a52c80954cd210fb0a09480
Yishak Kidanu <yisak04@gmail.com>
2022-09-23 06:08:25 -0700

    Script that takes a URL and displays response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# A script that takes a URL, and display error code
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print("Index")
