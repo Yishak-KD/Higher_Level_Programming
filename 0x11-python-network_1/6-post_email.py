5063a5053d69167053d360c0ffacd72f9bc0bb2a
Yishak Kidanu <yisak04@gmail.com>
2022-09-23 05:48:18 -0700

    POST request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Script that takes in a URL and email address and displays the body of the response
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
