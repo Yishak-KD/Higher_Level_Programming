8a5b0ca81413d03669a15a0e02874cc8c868d3cc
Yishak Kidanu <yisak04@gmail.com>
2022-09-23 11:05:22 -0700

    Python script that fetches using urllib package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,13 @@
 """
 using urllib package that fetches
 """
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        resp = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(resp)))
+        print("\t- content: {}".format(resp))
+        print("\t- utf8 content: {}".format(resp.decode('utf-8')))
