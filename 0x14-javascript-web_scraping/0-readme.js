bb8bc8d88bbcbd4dd6ba64588e25a4109e929769
Yishak Kidanu <yisak04@gmail.com>
2023-04-24 13:03:42 +0000

    A file system in Node to read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const fs = require('fs')
+const process = require('process')
+
+fs.readFile(`./${process.argv[2]}`, 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+	}
+	console.log(data);
 });
